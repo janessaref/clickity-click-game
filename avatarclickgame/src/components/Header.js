b06951dfd46dfe3887e1d1629c19ac34bff5f494
Game Over <janessarefong@gmail.com>
2020-11-11 00:33:52 -0800

    add background image

--- avatarclickgame/src/components/Header.js
+++ avatarclickgame/src/components/Header.js
@@ -1,18 +1,19 @@
 import React from "react";
 import "../styles/Header.css";
+// import background from "./public/images/aang.jpg"
 
-const styles = {
-  background: {
-    backgroundImage: "url('../../images/team.jpg')"
-  },
-};
+// var bgStyle = {
+//     width: "100%",
+//     height: "400px",
+//     backgroundImage: "url(" + { background } + ")"
+//   };
 
 // We use JSX curly braces to evaluate the style object
 
 function Header() {
   return (
     <div className="jumbotron jumbotron-fluid">
-  <div className="container" styles={styles.background}>
+  <div className="container">
     <h1 className="display-4 text-center mt-5">CLICK AN IMAGE TO BEGIN!</h1>
     <p className="lead text-center">Click an image to earn points, but don't click on the same character!</p>
   </div>
