ea9ebe5a8185768425a1d29ab66d344debb53989
Game Over <janessarefong@gmail.com>
2020-11-11 19:01:16 -0800

    fix margin and indenting in header component

--- avatarclickgame/src/components/Header/index.js
+++ avatarclickgame/src/components/Header/index.js
@@ -1,24 +1,15 @@
 import React from "react";
 import "./style.css";
-// import background from "./public/images/aang.jpg"
-
-// var bgStyle = {
-//     width: "100%",
-//     height: "400px",
-//     backgroundImage: "url(" + { background } + ")"
-//   };
-
-// We use JSX curly braces to evaluate the style object
 
 function Header() {
   return (
     <div className="jumbotron jumbotron-fluid">
-  <div className="container">
-    <h1 className="display-4 text-center mt-5">CLICK AN IMAGE TO BEGIN!</h1>
-    <p className="lead text-center">Click an image to earn points, but don't click on the same character!</p>
-  </div>
-</div>
+      <div className="container">
+        <h1 className="display-4 text-center mt-5">CLICK AN IMAGE TO BEGIN!</h1>
+        <p className="lead text-center">Click an image to earn points, but don't click on the same character!</p>
+      </div>
+    </div>
   );
-}
+};
 
 export default Header;
